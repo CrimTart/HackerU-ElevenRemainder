0451cfa1ce09f3d937eb4a3aa00f6c8af201a297
CrimTart <borisov.iv28@gmail.com>
2018-11-26 13:10:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         for (int i = 100; i < 1000; i++) {
             int c = i%10;
             int b = (i/10)%10;
-            int a = (i/100)%10;
+            int a = i/100;
             if ((i - a*a - b*b - c*c)%11 == 0) {
                 int q = (i - a*a - b*b - c*c)/11;
                 System.out.println(i + " = " + q + "*11 +" + a + "^2 + " + b + "^2 + " + c + "^2");
