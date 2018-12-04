7913fbe4451e63dbf98631813d3a6b206f325817
CrimTart <borisov.iv28@gmail.com>
2018-12-04 12:01:16 +0300

    Correct bounds & if clause

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     //abc = 11*q + a^2 + b^2 + c^2 (a,b,c are digits)
     public static void main(String[] args){
-        for (int i = 100; i < 1000; i++) {
+        for (int i = 100; i < 310; i++) {
             int c = i%10;
             int b = (i/10)%10;
             int a = i/100;
-            if ((i - a*a - b*b - c*c)%11 == 0) {
+            if (i%11 == a*a + b*b + c*c) {
                 int q = (i - a*a - b*b - c*c)/11;
                 System.out.println(i + " = " + q + "*11 +" + a + "^2 + " + b + "^2 + " + c + "^2");
             }
